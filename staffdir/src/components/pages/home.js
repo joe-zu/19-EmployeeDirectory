f090d48d85a8607c34260db4bccbbf027c96207e
Joe Giampaoli <giampaoli.joe@gmail.com>
2021-03-05 17:49:23 -0800

    fixed useEffect

--- staffdir/src/components/pages/home.js
+++ staffdir/src/components/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import StaffCard from "../StaffCard";
 import Wrapper from "../Wrapper";
 import Title from "../Title";
